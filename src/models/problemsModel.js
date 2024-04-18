d69c395d6133b5981cee88d51035407ba96923b4
OKPooja <poojagawade213046@tsecedu.org>
2024-04-19 01:10:00 +0530

    fetch problems

--- src/models/problemsModel.js
+++ src/models/problemsModel.js
@@ -1,9 +1,37 @@
 const mongoose = require('mongoose');
 
-const problemSchema = new mongoose.Schema({
-    problem: String,
-    difficulty: String,
-    acceptance: String
+const problemSchema = mongoose.Schema({
+    id:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    problem_name:{
+        type: String,
+        required: true,
+    },
+    problem_description:{
+        type: String,
+        required: true,
+    },
+    problem_difficulty:{
+        type: String,
+    },
+    solved:{
+        type: Boolean,
+    },
+    solution: {
+      type: String,  
+    },
+    solution_language:{
+        type: String,
+    },
+    bookmarked:{
+        type: Boolean,
+    },
+    category: {
+        type: String,
+    },
 });
 
-module.exports = mongoose.model("Problems", problemSchema);
+module.exports = mongoose.model("problems-list", problemSchema);
